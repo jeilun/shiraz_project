d131b37afd230846649800a174e8bf6d6ab46f39
jeilun <mrparnian@live.com>
2013-10-11 23:58:52 -0800

    test2

--- app/filters.php
+++ app/filters.php
@@ -6,7 +6,7 @@
 |--------------------------------------------------------------------------
 |
 | Below you will find the "before" and "after" events for the application
-| which may be used to do any work before or after a request into your
+|  which may be used to do any work before or after a request into your
 | application. Here you may also register your custom route filters.
 |
 */
